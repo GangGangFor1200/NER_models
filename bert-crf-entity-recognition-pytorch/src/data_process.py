a800962b0b31d132374e0b74af8d49d87676b7b6
Moon Yewan <mool2480@gmail.com>
2021-08-16 16:58:32 +0900

    add bert-crf-entity(fixed), BERT-BiLSTM-CRF-NER

---
+++ bert-crf-entity-recognition-pytorch/src/data_process.py
@@ -0,0 +1,228 @@
+from tqdm import tqdm
+from transformers import *
+
+import argparse
+import os
+import json
+
+
+def load_file(full_path, tokenizer):
+    total_tokens = []
+    total_tags = []
+
+    with open(full_path, 'r', encoding='UTF8') as f:
+        lines = f.readlines()
+
+    dialogue_tokens = []
+    dialogue_tags = []
+    for i, line in enumerate(tqdm(lines)):
+        if line.strip() != config['dialogue_split']:
+            comps = line.strip().split('\t')
+            speaker = comps[0]
+            sent = comps[1]
+
+            tokens = tokenizer.tokenize(sent)
+            entity_tags = ['O'] * len(tokens)
+
+            if len(comps) > 2:
+
+                entities = comps[2]
+
+                tokens, entity_tags = find_entities(
+                    tokens, entities, entity_tags, tokenizer)
+
+            dialogue_tokens.append(['[' + speaker + ']'] + tokens)
+            dialogue_tags.append(entity_tags)
+        else:
+            total_tokens.append(dialogue_tokens)
+            total_tags.append(dialogue_tags)
+            dialogue_tokens = []
+            dialogue_tags = []
+
+    return total_tokens, total_tags  # (N, T, L), (N, T, L)
+
+
+def find_entities(tokens, entities, entity_tags, tokenizer):
+    entity_list = entities.split(config['outer_split_symbol'])
+    checked = [False] * len(tokens)
+
+    for entity in entity_list:
+        print(entity)
+        entity_name = entity.split(config['inner_split_symbol'])[0]
+        entity_tag = entity.split(config['inner_split_symbol'])[1]
+
+        entity_tokens = tokenizer.tokenize(entity_name)
+
+        entity_tags, checked = find_sublist(
+            tokens, entity_tokens, entity_tag, entity_tags, checked)
+
+    return tokens, entity_tags
+
+
+def find_sublist(full, sub, tag, entity_tags, checked):
+    for i, e in enumerate(full):
+        if e == sub[0] and not checked[i]:
+            cand = full[i:i+len(sub)]
+
+            if cand == sub:
+                checked[i] = True
+                entity_tags[i] = f'B-{tag}'
+
+                if f'B-{tag}' not in class_dict:
+                    class_dict[f'B-{tag}'] = len(class_dict)
+                    class_dict[f'I-{tag}'] = len(class_dict)
+                    class_dict[f'E-{tag}'] = len(class_dict)
+
+                if len(sub) > 1:
+                    entity_tags[i+len(sub)-1] = f'E-{tag}'
+                    entity_tags = [f'I-{tag}' if cur_tag == 'O' and (j > i and j < i+len(
+                        sub)) else cur_tag for j, cur_tag in enumerate(entity_tags)]
+
+    return entity_tags, checked
+
+
+def save_text(full_dir, dialogues, dialogue_labels):
+    if not os.path.isdir(full_dir):
+        os.mkdir(full_dir)
+
+    with open(f"{full_dir}/inputs.txt", 'w', encoding='UTF8') as f:
+        for d, dialogue_tokens in enumerate(tqdm(dialogues)):
+            history = []
+            for t, tokens in enumerate(dialogue_tokens):
+                speaker = tokens[0]
+                sent = ' '.join(tokens[1:])
+                utter = f"{speaker}\t{sent}"
+
+                if t < config['max_time']:
+                    history.append(utter)
+                else:
+                    history = history[1:] + [utter]
+
+                f.write(f"{config['utter_split'].join(history)}\n")
+
+    with open(f"{full_dir}/labels.txt", 'w', encoding='UTF8') as f:
+        for d, dialogue_tags in enumerate(tqdm(dialogue_labels)):
+            for t, tags in enumerate(dialogue_tags):
+                label = ' '.join(tags)
+                f.write(f"{label}\n")
+
+
+def save_ids(full_dir, dialogues, dialogue_labels):
+    if not os.path.isdir(full_dir):
+        os.mkdir(full_dir)
+
+    with open(f"{full_dir}/inputs.ids", 'w') as f:
+        for d, dialogue_tokens in enumerate(tqdm(dialogues)):
+            history = []
+            for t, tokens in enumerate(dialogue_tokens):
+                speaker = tokens[0]
+                sent = [str(tokenizer._convert_token_to_id(token))
+                        for token in tokens[1:]]
+                sent = ' '.join(sent)
+                utter = f"{speaker}\t{sent}"
+
+                if t < config['max_time']:
+                    history.append(utter)
+                else:
+                    history = history[1:] + [utter]
+
+                f.write(f"{config['utter_split'].join(history)}\n")
+
+    with open(f"{full_dir}/labels.ids", 'w', encoding='UTF8') as f:
+        for d, dialogue_tags in enumerate(tqdm(dialogue_labels)):
+            for t, tags in enumerate(dialogue_tags):
+                label = [str(class_dict[tag]) for tag in tags]
+                label = ' '.join(label)
+                f.write(f"{label}\n")
+
+
+def count_utter_num(tokens):
+    count = 0
+    for dialogue in tokens:
+        count += len(dialogue)
+
+    return count
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', required=True,
+                        type=str, help="The path to configuration file.")
+
+    args = parser.parse_args()
+
+    print("Loading the configurations...")
+    with open(args.config_path, 'r') as f:
+        config = json.load(f)
+
+    print("Loading the tokenizer...")
+    tokenizer = BertTokenizer.from_pretrained(config['bert_name'])
+
+    total_train_tokens = []
+    total_train_tags = []
+    total_valid_tokens = []
+    total_valid_tags = []
+    total_test_tokens = []
+    total_test_tags = []
+
+    class_dict = {'O': 0}
+
+    file_list = os.listdir(f"{config['data_dir']}/{config['original_dir']}")
+    for file in file_list:
+        if os.path.isfile(f"{config['data_dir']}/{config['original_dir']}/{file}"):
+            print(f"Processing {file}...")
+            tokens, tags = load_file(
+                f"{config['data_dir']}/{config['original_dir']}/{file}", tokenizer)
+            train_tokens, train_tags = tokens[:int(
+                len(tokens)*config['train_frac'])], tags[:int(len(tags)*config['train_frac'])]
+            remained_tokens, remained_tags = tokens[int(
+                len(tokens)*config['train_frac']):], tags[int(len(tags)*config['train_frac']):]
+
+            f = config['valid_frac'] / (1.0-config['train_frac'])
+            valid_tokens, valid_tags = remained_tokens[:int(
+                len(remained_tokens)*f)], remained_tags[:int(len(remained_tags)*f)]
+            test_tokens, test_tags = remained_tokens[int(
+                len(remained_tokens)*f):], remained_tags[int(len(remained_tags)*f):]
+
+            total_train_tokens += train_tokens
+            total_train_tags += train_tags
+            total_valid_tokens += valid_tokens
+            total_valid_tags += valid_tags
+            total_test_tokens += test_tokens
+            total_test_tags += test_tags
+
+    train_utter_num = count_utter_num(total_train_tokens)
+    valid_utter_num = count_utter_num(total_valid_tokens)
+    test_utter_num = count_utter_num(total_test_tokens)
+
+    full_dir = f"{config['data_dir']}/{config['entity_dir']}"
+    if not os.path.isdir(full_dir):
+        os.mkdir(full_dir)
+
+    print("Making the label dictionary...")
+    with open(f"{full_dir}/{config['tags_name']}.json", 'w') as f:
+        json.dump(class_dict, f)
+
+    print("Saving each data files...")
+    save_text(f"{full_dir}/{config['train_name']}",
+              total_train_tokens, total_train_tags)
+    save_ids(f"{full_dir}/{config['train_name']}",
+             total_train_tokens, total_train_tags)
+    save_text(f"{full_dir}/{config['valid_name']}",
+              total_valid_tokens, total_valid_tags)
+    save_ids(f"{full_dir}/{config['valid_name']}",
+             total_valid_tokens, total_valid_tags)
+    save_text(f"{full_dir}/{config['test_name']}",
+              total_test_tokens, total_test_tags)
+    save_ids(f"{full_dir}/{config['test_name']}",
+             total_test_tokens, total_test_tags)
+
+    print("Finished!")
+
+    print("######################################## Anaysis ########################################")
+    print(f"The number of train dialogues: {len(total_train_tokens)}")
+    print(f"The number of valid dialogues: {len(total_valid_tokens)}")
+    print(f"The number of test dialogues: {len(total_test_tokens)}")
+    print(f"The number of train utterances: {train_utter_num}")
+    print(f"The number of valid utterances: {valid_utter_num}")
+    print(f"The number of test utterances: {test_utter_num}")
---
+++ bert-crf-entity-recognition-pytorch/src/main.py
@@ -0,0 +1,306 @@
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+from entity_bert import *
+from custom_dataset import *
+from transformers import *
+from sklearn.metrics import *
+from itertools import chain
+
+import torch
+import os
+import sys
+import numpy as np
+import argparse
+import time
+import copy
+import json
+
+
+class Manager():
+    def __init__(self, mode, config_path, ckpt_name=None):
+        print("Setting the configurations...")
+        with open(args.config_path, 'r') as f:
+            self.config = json.load(f)
+
+        if self.config['device'] == "cuda":
+            self.config['device'] = torch.device(
+                'cuda') if torch.cuda.is_available() else torch.device('cpu')
+        elif self.config['device'] == "cpu":
+            self.config['device'] = torch.device('cpu')
+
+        self.config['mode'] = mode
+
+        assert self.config['turn_type'] == 'single' or self.config['turn_type'] == 'multi', print(
+            "Please specify a correct turn type, either 'single' or 'multi'.")
+        print(f"{self.config['turn_type'].title()}-turn setting fixed.")
+        if self.config['turn_type'] == 'multi':
+            assert self.config['sentence_embedding'] == 'cls' or self.config['sentence_embedding'] == 'max' or self.config['sentence_embedding'] == 'mean', \
+                print(
+                    "Please specify a correct sentence embedding method among these three, 'cls', 'max' and 'mean'.")
+            print(
+                f"Sentence embedding policy is {self.config['sentence_embedding']}.")
+
+        bert_config = BertConfig().from_pretrained(self.config['bert_name'])
+        # 수정
+        self.config['hidden_size'] = bert_config.hidden_size
+        self.config['p_dim'] = self.config['hidden_size']
+
+        # Tokenizer & Vocab
+        print("Loading the tokenizer...")
+        self.tokenizer = BertTokenizer.from_pretrained(
+            self.config['bert_name'])
+        num_new_tokens = self.tokenizer.add_special_tokens(
+            {
+                'additional_special_tokens': [self.config['speaker1_token'], self.config['speaker2_token']]
+            }
+        )
+        self.config['vocab_size'] = len(self.tokenizer.get_vocab())
+
+        # Load class dictionary
+        print("Loading the class dictionary...")
+        full_dir = f"{self.config['data_dir']}/{self.config['entity_dir']}"
+        with open(f"{full_dir}/{self.config['tags_name']}.json", 'r') as f:
+            class_dict = json.load(f)
+        self.config['num_classes'] = len(class_dict)
+
+        # Load model & optimizer
+        print("Loading the model and optimizer...")
+        self.model = EntityBert(self.config).to(self.config['device'])
+        self.optim = torch.optim.AdamW(
+            self.model.parameters(), lr=self.config['learning_rate'])
+        self.best_f1 = 0.0
+
+        if not os.path.exists(self.config['ckpt_dir']):
+            os.mkdir(self.config['ckpt_dir'])
+
+        if ckpt_name is not None:
+            assert os.path.exists(
+                f"{self.config['ckpt_dir']}/{ckpt_name}"), f"There is no checkpoint named {ckpt_name}."
+
+            print("Loading checkpoint...")
+            checkpoint = torch.load(f"{self.config['ckpt_dir']}/{ckpt_name}")
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optim.load_state_dict(checkpoint['optim_state_dict'])
+            self.best_f1 = checkpoint['f1']
+        else:
+            print("Initializing the model...")
+            self.model.init_model()
+
+        print(f"Loading {self.config['turn_type']}-turn data...")
+        if mode == 'train':
+            # Load train & valid dataset
+            train_set = CustomDataset(
+                f"{full_dir}/{self.config['train_name']}", self.tokenizer, self.config, class_dict)
+            valid_set = CustomDataset(
+                f"{full_dir}/{self.config['valid_name']}", self.tokenizer, self.config, class_dict)
+            self.train_loader = DataLoader(
+                train_set, shuffle=True, batch_size=self.config['batch_size'])
+            self.valid_loader = DataLoader(
+                valid_set, shuffle=True, batch_size=self.config['batch_size'])
+        elif mode == 'test':
+            # Load test dataset
+            test_set = CustomDataset(
+                f"{full_dir}/{self.config['test_name']}", self.tokenizer, self.config, class_dict)
+            self.test_loader = DataLoader(
+                test_set, shuffle=True, batch_size=self.config['batch_size'])
+
+        print("Setting finished.")
+
+    def train(self):
+        print("Training starts.")
+
+        for epoch in range(1, self.config['num_epochs']+1):
+            self.model.train()
+
+            print(f"#################### Epoch: {epoch} ####################")
+            train_losses = []
+            train_pred = []
+            train_true = []
+            for i, batch in enumerate(tqdm(self.train_loader)):
+                batch_x, batch_y, batch_lens, batch_times = batch
+                batch_times = batch_times.squeeze(-1)  # (B)
+                batch_lens = batch_lens.squeeze(-1)  # (B)
+                pad_id = self.tokenizer._convert_token_to_id(
+                    self.config['pad_token'])
+
+                if self.config['turn_type'] == 'single':
+                    batch_x, batch_y = batch_x.to(
+                        self.config['device']), batch_y.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y)  # (), (B, L)
+                elif self.config['turn_type'] == 'multi':
+                    batch_x, batch_y, batch_times = \
+                        batch_x.to(self.config['device']), batch_y.to(
+                            self.config['device']), batch_times.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y, times=batch_times)  # (), (B, L)
+
+                loss = -1 * log_likelihood
+
+                self.model.zero_grad()
+                self.optim.zero_grad()
+
+                loss.backward()
+                self.optim.step()
+
+                train_losses.append(loss.item())
+                pred_list = list(chain.from_iterable(tag_seq))
+                train_pred += pred_list
+                true_list = list(chain.from_iterable(
+                    [sublist[:batch_lens.tolist()[b]] for b, sublist in enumerate(batch_y.tolist())]))
+                train_true += true_list
+
+                assert len(pred_list) == len(
+                    true_list), "Please check if the length of predictions and that of true labels are identical."
+
+            mean_train_loss = np.mean(train_losses)
+            train_acc = accuracy_score(train_true, train_pred)
+            train_f1 = f1_score(train_true, train_pred, average='weighted')
+            print(
+                f"Train loss: {mean_train_loss} || Train accuracy: {train_acc} || Train F1 score: {train_f1}")
+
+            valid_loss, valid_acc, valid_f1 = self.validation()
+
+            if valid_f1 > self.best_f1:
+                self.best_f1 = valid_f1
+
+                state_dict = {
+                    'model_state_dict': self.model.state_dict(),
+                    'optim_state_dict': self.optim.state_dict(),
+                    'f1': self.best_f1
+                }
+
+                torch.save(
+                    state_dict, f"{self.config['ckpt_dir']}/best_ckpt.tar")
+                print(f"***** Current best checkpoint is saved. *****")
+
+            print(f"Best validtion f1 score: {self.best_f1}")
+            print(
+                f"Validation loss: {valid_loss} || Validation accuracy: {valid_acc} || Current validation F1 score: {valid_f1}")
+
+        print("Training finished!")
+
+    def validation(self):
+        print("Validation processing...")
+        self.model.eval()
+
+        valid_losses = []
+        valid_pred = []
+        valid_true = []
+        with torch.no_grad():
+            for i, batch in enumerate(tqdm(self.valid_loader)):
+                batch_x, batch_y, batch_lens, batch_times = batch
+                batch_times = batch_times.squeeze(-1)  # (B)
+                batch_lens = batch_lens.squeeze(-1)  # (B)
+                pad_id = self.tokenizer._convert_token_to_id(
+                    self.config['pad_token'])
+
+                if self.config['turn_type'] == 'single':
+                    batch_x, batch_y = batch_x.to(
+                        self.config['device']), batch_y.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y)  # (), (B, L)
+                elif self.config['turn_type'] == 'multi':
+                    batch_x, batch_y, batch_times = \
+                        batch_x.to(self.config['device']), batch_y.to(
+                            self.config['device']), batch_times.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y, times=batch_times)  # (), (B, L)
+
+                loss = -1 * log_likelihood
+
+                valid_losses.append(loss.item())
+                pred_list = list(chain.from_iterable(tag_seq))
+                valid_pred += pred_list
+                true_list = list(chain.from_iterable(
+                    [sublist[:batch_lens.tolist()[b]] for b, sublist in enumerate(batch_y.tolist())]))
+                valid_true += true_list
+
+                assert len(pred_list) == len(
+                    true_list), "Please check if the length of predictions and that of true labels are identical."
+
+        mean_valid_loss = np.mean(valid_losses)
+        valid_acc = accuracy_score(valid_true, valid_pred)
+        valid_f1 = f1_score(valid_true, valid_pred, average='weighted')
+
+        return mean_valid_loss, valid_acc, valid_f1
+
+    def test(self):
+        print("Testing starts.")
+        self.model.eval()
+
+        test_losses = []
+        test_pred = []
+        test_true = []
+        with torch.no_grad():
+            for i, batch in enumerate(tqdm(self.test_loader)):
+                batch_x, batch_y, batch_lens, batch_times = batch
+                batch_times = batch_times.squeeze(-1)  # (B)
+                batch_lens = batch_lens.squeeze(-1)  # (B)
+                pad_id = self.tokenizer._convert_token_to_id(
+                    self.config['pad_token'])
+
+                if self.config['turn_type'] == 'single':
+                    batch_x, batch_y = batch_x.to(
+                        self.config['device']), batch_y.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y)  # (), (B, L)
+                elif self.config['turn_type'] == 'multi':
+                    batch_x, batch_y, batch_times = \
+                        batch_x.to(self.config['device']), batch_y.to(
+                            self.config['device']), batch_times.to(self.config['device'])
+                    log_likelihood, tag_seq = self.model(
+                        batch_x, pad_id, batch_y, times=batch_times)  # (), (B, L)
+
+                loss = -1 * log_likelihood
+
+                test_losses.append(loss.item())
+                pred_list = list(chain.from_iterable(tag_seq))
+                test_pred += pred_list
+                true_list = list(chain.from_iterable(
+                    [sublist[:batch_lens.tolist()[b]] for b, sublist in enumerate(batch_y.tolist())]))
+                test_true += true_list
+
+                assert len(pred_list) == len(
+                    true_list), "Please check if the length of predictions and that of true labels are identical."
+
+        mean_test_loss = np.mean(test_losses)
+        test_acc = accuracy_score(test_true, test_pred)
+        test_f1 = f1_score(test_true, test_pred, average='weighted')
+
+        print("#################### Test results ####################")
+        print(
+            f"Test loss: {mean_test_loss} || Test accuracy: {test_acc} || Test F1 score: {test_f1}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', required=True,
+                        type=str, help="The path to configuration file.")
+    parser.add_argument('--mode', required=True,
+                        type=str, help="Train or test?")
+    parser.add_argument('--ckpt_name', required=False,
+                        type=str, help="Best checkpoint file.")
+
+    args = parser.parse_args()
+
+    assert args.mode == 'train' or args.mode == 'test', print(
+        "Please specify a correct mode name, 'train' or 'test'.")
+
+    if args.mode == 'train':
+        if args.ckpt_name is not None:
+            manager = Manager(args.mode, args.config_path,
+                              ckpt_name=args.ckpt_name)
+        else:
+            manager = Manager(args.mode, args.config_path)
+
+        manager.train()
+
+    elif args.mode == 'test':
+        assert args.ckpt_name is not None, "Please specify the trained model checkpoint."
+
+        manager = Manager(args.mode, args.config_path,
+                          ckpt_name=args.ckpt_name)
+
+        manager.test()
